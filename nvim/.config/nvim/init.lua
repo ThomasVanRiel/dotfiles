eab37cb6b6ecca5ea9593bacd2233b0ff302e52b
Thomas Van Riel <thomas.van.riel@gmail.com>
2026-01-27 19:51:32 +0100

    Ranger config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -49,7 +49,7 @@ require("lazy").setup({
           strings = {},
           variables = {},
           numbers = {},
-          booleans = {},
+ booleans = {},
           properties = {},
           types = {},
           operators = {},
@@ -93,4 +93,13 @@ require("lazy").setup({
       vim.cmd.colorscheme("catppuccin")
     end,
   },
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' },            -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+}
 })
