1139de2ba910f6896a7de60530fd36cd487d2d71
Thomas Van Riel <thomas.van.riel@gmail.com>
2026-02-06 13:04:56 +0100

    Added neotree plugin

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -101,5 +101,16 @@ require("lazy").setup({
     ---@module 'render-markdown'
     ---@type render.md.UserConfig
     opts = {},
-}
+},
+{"https://github.com/Weyaaron/nvim-training", pin= true, opts = {}},
+{
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons", -- optional, but recommended
+    },
+    lazy = false, -- neo-tree will lazily load itself
+  }
 })
