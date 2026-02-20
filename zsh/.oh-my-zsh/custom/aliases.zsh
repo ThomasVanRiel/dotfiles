d5d7acdc6ac30dc6ca4e15a9346be38dfbd3dd7e
Thomas Van Riel <thomas.van.riel@gmail.com>
2026-02-20 11:07:13 +0100

    .

--- zsh/.oh-my-zsh/custom/aliases.zsh
+++ zsh/.oh-my-zsh/custom/aliases.zsh
@@ -1,3 +1,10 @@
 alias ta="tmux a"
 alias claudecommit='claude --model haiku "/commit"'
 alias dps='docker ps -a --format "table {{.Names}}\t{{.Status}}"'
+alias e='eza --long --links --group --color=auto --git --git-repos' 
+alias ea='e --absolute'
+alias ets='e --total-size'
+alias eats='ea --total-size'
+alias etr='e --sort new'
+alias et='eza --tree --level=2 --long --git'
+alias eta='et --absolute'
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -118,3 +118,4 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 [ -f "$HOME/.local/bin/env" ] && . "$HOME/.local/bin/env"
+. "$HOME/.cargo/env"
