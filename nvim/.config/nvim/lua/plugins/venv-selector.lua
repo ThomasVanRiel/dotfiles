e17a62e4ca1f5e8431236b7d56a922a5274d8db6
Thomas Van Riel <thomas.van.riel@gmail.com>
2026-03-12 13:19:18 +0100

    nvim pyenv

---
+++ nvim/.config/nvim/lua/plugins/venv-selector.lua
@@ -0,0 +1,8 @@
+return {
+  "linux-cultist/venv-selector.nvim",
+  ft = "python",
+  keys = { { "<leader>cv", "<cmd>VenvSelect<cr>", desc = "Select VirtualEnv" } },
+  opts = {
+    picker = "snacks",
+  },
+}
