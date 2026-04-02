948227c708728c421371876a04af4d612ce81856
Thomas Van Riel <thomas.van.riel@gmail.com>
2026-04-02 21:49:47 +0200

    Changes

--- zsh/.oh-my-zsh/custom/aliases.zsh
+++ zsh/.oh-my-zsh/custom/aliases.zsh
@@ -2,5 +2,5 @@ alias ta="tmux a"
 alias claudecommit='claude --model haiku "/commit"'
 alias dps='docker ps -a --format "table {{.Names}}\t{{.Status}}"'
 alias e='eza --long --links --group --color=auto --git --git-repos' 
-alias etree='eza --tree --level=2 --long --git'
+alias etree='e --tree --level=2'
 alias gdt='git difftool'
