f92adb6a5bcba8ed7d9bdafb2f27687737c2ae30
Thomas Van Riel <thomas.van.riel@gmail.com>
2026-04-08 14:01:53 +0200

    Update nvim to 0.12

---
+++ nvim/.config/nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    ft = { "markdown", "norg", "rmd", "org", "codecompanion", "quarto" },
+    opts = {
+      file_types = { "markdown", "norg", "rmd", "org", "codecompanion", "quarto" },
+    },
+  },
+}
