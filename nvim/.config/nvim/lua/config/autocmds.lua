9a9f792af50efc200c129a57f5f112356823aa7b
Thomas Van Riel <thomas.van.riel@gmail.com>
2026-04-09 00:17:38 +0200

    fixes

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -7,13 +7,6 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
--- Reset cursor to blinking bar on exit (Neovim leaves a block cursor)
-vim.api.nvim_create_autocmd("VimLeave", {
-  callback = function()
-    io.write("\27[5 q")
-    io.flush()
-  end,
-})
 
 -- Start Godot Neovim server (cleans up stale socket)
 vim.api.nvim_create_user_command("GodotServer", function()
