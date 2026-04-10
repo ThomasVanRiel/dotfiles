3838c5a6b71c9462406afc43dc0b7b063a229cfb
Thomas Van Riel <thomas.van.riel@gmail.com>
2026-04-10 23:26:47 +0200

    Update lock script and screen

--- scripts/.config/scripts/lock.sh
+++ scripts/.config/scripts/lock.sh
@@ -2,14 +2,10 @@
 # Pixelated lock screen wallpaper script
 # Takes screenshot, pixelates it, sets it as the noctalia wallpaper, then locks
 
-SCREENSHOT="/tmp/lockscreen-raw.png"
 PIXELATED="/tmp/lockscreen.png"
 
-# Take screenshot
-grim "$SCREENSHOT"
-
-# Pixelate: scale down then back up (lower = more pixelated)
-magick "$SCREENSHOT" -scale 3% -scale 2000% "$PIXELATED"
+# Take screenshot and pixelate in one step
+grim - | magick - -scale 3% -scale 2000% "$PIXELATED"
 
 # Set as wallpaper before locking
 qs -c noctalia-shell ipc call wallpaper set "$PIXELATED" ""
