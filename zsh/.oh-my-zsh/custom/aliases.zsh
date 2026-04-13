7c4667af97ebffbd6021c82e15c20f59b5282194
Thomas Van Riel <thomas.van.riel@gmail.com>
2026-04-13 08:58:39 +0200

    Add venv toggle alias

--- zsh/.oh-my-zsh/custom/aliases.zsh
+++ zsh/.oh-my-zsh/custom/aliases.zsh
@@ -5,3 +5,10 @@ alias e='eza --long --links --group --color=auto --git --git-repos'
 alias ea='e -a'
 alias etree='e --tree --level=2'
 alias gdt='git difftool'
+venv() {
+  if [[ -n "$VIRTUAL_ENV" ]]; then
+    deactivate
+  else
+    source .venv/bin/activate
+  fi
+}
