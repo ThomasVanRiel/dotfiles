1f1832f6df91a2739027d39fc85e8ce0d5dbc9ca
Thomas Van Riel <thomas.van.riel@gmail.com>
2026-06-29 14:27:54 +0200

    Nvim keybind for copying file path and root dir ( and ).

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,16 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Copy paths to the system clipboard
+vim.keymap.set("n", "<leader>fy", function()
+  local path = vim.fn.expand("%:p")
+  vim.fn.setreg("+", path)
+  vim.notify("Copied file path: " .. path)
+end, { desc = "Copy file path" })
+
+vim.keymap.set("n", "<leader>fY", function()
+  local dir = vim.fn.getcwd()
+  vim.fn.setreg("+", dir)
+  vim.notify("Copied cwd: " .. dir)
+end, { desc = "Copy working directory" })
