92649b3d2f0f0f0fcbd644ae85a6d62738261b03
Thomas Van Riel <thomas.van.riel@gmail.com>
2026-06-30 08:45:26 +0200

    Add  keymap to copy  or  in visual mode

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -14,3 +14,21 @@ vim.keymap.set("n", "<leader>fY", function()
   vim.fn.setreg("+", dir)
   vim.notify("Copied cwd: " .. dir)
 end, { desc = "Copy working directory" })
+
+vim.keymap.set({ "n", "x" }, "<leader>fl", function()
+  local path = vim.fn.expand("%:.")
+  local ref
+  if vim.fn.mode():match("[vV\22]") then
+    -- visual mode: copy a line range (start-end)
+    local s, e = vim.fn.line("v"), vim.fn.line(".")
+    if s > e then
+      s, e = e, s
+    end
+    ref = path .. ":" .. s .. (s ~= e and "-" .. e or "")
+  else
+    -- normal mode: copy the current line
+    ref = path .. ":" .. vim.fn.line(".")
+  end
+  vim.fn.setreg("+", ref)
+  vim.notify("Copied file ref: " .. ref)
+end, { desc = "Copy file path with line(s)" })
