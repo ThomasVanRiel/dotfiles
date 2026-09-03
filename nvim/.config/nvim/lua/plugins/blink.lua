8ebb06788ecd630cf21ec31885cebc0566e6b5eb
Thomas Van Riel <thomas.van.riel@gmail.com>
2026-09-03 07:55:34 +0200

    merge

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -1,7 +1,20 @@
 return {
   {
     "saghen/blink.cmp",
+    keys = {
+      {
+        "<leader>uq",
+        function()
+          vim.g.blink_enabled = vim.g.blink_enabled == false
+          vim.notify("blink.cmp " .. (vim.g.blink_enabled == false and "disabled" or "enabled"))
+        end,
+        desc = "Toggle blink.cmp",
+      },
+    },
     opts = {
+      enabled = function()
+        return vim.g.blink_enabled ~= false
+      end,
       keymap = {
         ["<CR>"] = {},
         ["<Tab>"] = { "select_and_accept", "snippet_forward", "fallback" },
--- nvim/.config/nvim/lua/plugins/quarto.lua
+++ nvim/.config/nvim/lua/plugins/quarto.lua
@@ -9,5 +9,18 @@ return {
     init = function()
       vim.treesitter.language.register("markdown", "quarto")
     end,
+    opts = { -- <- this is what was missing
+      lspFeatures = { languages = { "python" } },
+      codeRunner = { enabled = true, default_method = "slime" },
+    },
+  },
+  {
+    "jpalardy/vim-slime",
+    init = function()
+      vim.g.slime_target = "tmux"
+      vim.g.slime_bracketed_paste = 1 -- required, or ipython mangles indented blocks
+      vim.g.slime_default_config = { socket_name = "default", target_pane = "{last}" }
+      vim.g.slime_dont_ask_default = 1
+    end,
   },
 }
